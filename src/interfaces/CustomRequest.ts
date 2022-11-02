ef809b6d9cc445d37b8d46b8e34413fa228c2c4e
Mike Valeriano <mkvlrn@gmail.com>
2022-11-02 04:57:36 -0300

    feat: auth interface

--- src/interfaces/CustomRequest.ts
+++ src/interfaces/CustomRequest.ts
@@ -1,11 +1,13 @@
 import { Request } from 'express';
 
+import { AuthPayload } from '#/interfaces/AuthPayload';
+
 export interface CustomRequest<
   B = any,
   P extends string = any,
   Q extends string = any,
 > extends Request {
-  user?: { id: string; email: string };
+  user?: AuthPayload;
   body: B;
   params: { [key in P]: string };
   query: { [key in Q]: string };
